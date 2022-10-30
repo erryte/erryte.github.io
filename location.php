0f98f9873c6ddcfd64294e820c51bd6530230b33
Terry <56442073+erryte@users.noreply.github.com>
2022-10-30 12:35:15 -0700

    Delete location.php

--- location.php
+++
@@ -1,5 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php include('components/navbar.inc.php'); ?>
-<?php include('components/header.inc.php'); ?>
-<?php include('components/location.inc.php'); ?>
-<?php include('components/footer.inc.php'); ?>
