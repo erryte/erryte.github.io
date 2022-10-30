0997494ee657d77c6e8bf0cf68ab5654b20c3e96
Terry <56442073+erryte@users.noreply.github.com>
2022-10-30 12:35:21 -0700

    Delete products.php

--- products.php
+++
@@ -1,5 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php include('components/navbar.inc.php'); ?>
-<?php include('components/header.inc.php'); ?>
-<?php include('components/products.inc.php'); ?>
-<?php include('components/footer.inc.php'); ?>
